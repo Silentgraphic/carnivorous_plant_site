481054beeb7d7ae7c5edf8ddc9f544905b0a24cc
silentgraphic <kierannolan1995@gmail.com>
2020-09-09 12:15:50 +0100

    Added the plant info page

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -1,3 +1,7 @@
 exports.plants_home = (req, res) => {
     res.render('cp_plants_home');
-}
+};
+
+exports.plants_info =(req,res) => {
+    res.render('plant_info_layout', {plant_name: req.params.plant_name});
+};
--- routes/plants.js
+++ routes/plants.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/*GET plants page*/
 const plants = require('../controllers/cp_plants_controller');
 
+/*GET plants home page*/
 router.get('/', plants.plants_home);
+
+/*GET plant info page*/
+router.get('/:plant_name', plants.plants_info );
 
 module.exports = router;
