d076c0f6b8ea52b4074a8be86bfe1ace28aee34e
silentgraphic <kierannolan1995@gmail.com>
2020-09-14 11:25:22 +0100

    changed test_app to test_routes and added route tests

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -2,6 +2,6 @@ exports.plants_home = (req, res) => {
     res.render('cp_plants_home');
 };
 
-exports.plants_info =(req,res) => {
-    res.render('plant_info_layout', {plant_name: req.params.plant_name});
+exports.plants_info = (req, res) => {
+    res.render('plant_info_layout', { plant_name: req.params.plant_name });
 };
--- test/test_controllers/test_index_controller.js
+++ test/test_controllers/test_index_controller.js
@@ -1,2 +1,17 @@
-const assert = require('chai').assert;
+var expect = require('chai').expect;
+const express = require('express');
+const app = express();
 
+const index_controller = require('../../controllers/index_controller');
+
+
+describe('index controller', function () {
+    describe('index', function () {
+        it('calling index.index should return render function', function (done) {
+            var res = index_controller.index;
+            expect(res).to.be.a('function');
+            done();
+        });
+    });
+
+});
