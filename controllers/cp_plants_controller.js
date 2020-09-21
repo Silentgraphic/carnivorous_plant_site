b1d24dd12479369e828f5cda2e9e13f46011c024
silentgraphic <kierannolan1995@gmail.com>
2020-09-21 16:06:48 +0100

    Changed plant_info model and set up testing code

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -1,7 +1,14 @@
-exports.plants_home = (req, res) => {
+const PlantModel = require('../models/plant_info');
+
+exports.plants_home = (req, res, next) => {
     res.render('cp_plants_home');
 };
 
-exports.plants_info = (req, res) => {
+exports.plants_info = (req, res, next) => {
+    PlantModel.findOne({title:'Sarracenia'})
+        .exec(function (err, plant) {
+            if (err) return next(err);
+        });
+
     res.render('plant_info_layout', { plant_name: req.params.plant_name });
 };
--- models/plant_info.js
+++ models/plant_info.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
 
 const Plant_info_schema = new Schema({
     title: { type: String, required: true },
-    images: [string],
+    images: [String],
     plant_text: { type: String, required: true }
 });
 
 //Export model
-module.exports = mongoose.model('book_info', Plant_info_schema);
+module.exports = mongoose.model('plant_info', Plant_info_schema);
