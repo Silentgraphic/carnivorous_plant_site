a9e958ceed845fce33f9873e46e147275d94cc9c
silentgraphic <kierannolan1995@gmail.com>
2020-09-23 13:31:07 +0100

    Added validator and extracted callback

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -1,14 +1,20 @@
 const PlantModel = require('../models/plant_info');
 
+const { param } = require("express-validator");
+
+function plants_info_database_callback(err, plant) {
+    if (err) return next(err);
+}
+
 exports.plants_home = (req, res, next) => {
     res.render('cp_plants_home');
 };
 
-exports.plants_info = (req, res, next) => {
-    PlantModel.findOne({title:'Sarracenia'})
-        .exec(function (err, plant) {
-            if (err) return next(err);
-        });
-
+exports.plants_info = async (req, res, next) => {
+    //Escape plant name
+    await param('plant_name').escape().run(req);
+    
+    PlantModel.findOne({ title: req.params.plant_name }, plants_info_database_callback);
+    
     res.render('plant_info_layout', { plant_name: req.params.plant_name });
 };
