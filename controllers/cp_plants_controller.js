5f5d26a2172915912bd53b3a240ae80085434449
silentgraphic <kierannolan1995@gmail.com>
2020-09-28 14:57:25 +0100

    Changed the render info to private var instead of req.params

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -4,9 +4,17 @@ const createError = require('http-errors');
 const { param } = require("express-validator");
 const { create } = require('../models/plant_info');
 
-function plants_info_database_callback(err, plant, next) {
+let render_info = null;
+
+function set_render_info(info) {
+    render_info = info;
+    render_info.title = render_info.title.charAt(0).toUpperCase() + render_info.title.slice(1);
+}
+
+function plants_info_database_callback(err, plant) {
     if (err) throw err;
     if (plant === null) throw new Error('No plant named that');
+    else set_render_info(plant)
 }
 
 exports.plants_home = (req, res, next) => {
@@ -23,7 +31,7 @@ exports.plants_info = [
         PlantModel.findOne({ title: req.params.plant_name },
             (err, plant) => {
                 try {
-                    plants_info_database_callback(err, plant, next);
+                    plants_info_database_callback(err, plant);
                     next();
                 } catch (error) {
                     if (error.message == 'No plant named that') return next(createError(404));
@@ -32,8 +40,8 @@ exports.plants_info = [
             }
         );
     },
-    //Proccess request
-    (req, res, next) => {
-        res.render('plant_info_layout', { plant_name: req.params.plant_name });
+    //Render response
+    (req, res) => {
+        res.render('plant_info_layout', { title: render_info.title, plant_name: render_info.title, plant_text: render_info.plant_text });
     }
 ];
