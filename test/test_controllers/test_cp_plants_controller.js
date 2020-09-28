be15b2057c81b571a730982aea99212125541ff5
silentgraphic <kierannolan1995@gmail.com>
2020-09-28 17:41:51 +0100

    Changed test for plant_info from fucntion to array

--- test/test_controllers/test_cp_plants_controller.js
+++ test/test_controllers/test_cp_plants_controller.js
@@ -1,5 +1,8 @@
 const expect = require('chai').expect;
 const rewire = require('rewire');
+const chai = require('chai');
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays);
 
 const cp_plants_controller = rewire('../../controllers/cp_plants_controller');
 
@@ -19,13 +22,16 @@ describe('plants controller', function () {
         Req_object = {
             params: { plant_name: null },
         };
+        //Remove sanitizer as it an external dependency that doesn't need testing
+        cp_plants_controller.plants_info = cp_plants_controller.plants_info.splice(1);
+        console.log(cp_plants_controller.plants_info);
     });
     describe('plants', function () {
         it('calling .plants_home should return render function', function (done) {
             expect(cp_plants_controller.plants_home).to.be.a('function');
             done();
         });
-        
+
         it('calling .plants_home should render cp_plants_home', function () {
             var res = Res_object;
             cp_plants_controller.plants_home(null, res);
@@ -33,13 +39,21 @@ describe('plants controller', function () {
         });
     });
     describe('plants info', function () {
-        it('calling .plants_info should return render function', function (done) {
-            expect(cp_plants_controller.plants_info).to.be.a('function');
-            done();
+        describe('plants_info should be an array of callback functions', function () {
+            it('calling .plants_info should return array of callbacks', function (done) {
+                expect(cp_plants_controller.plants_info).to.be.an('array');
+                done();
+            });
+            it('elements of the array are fucntions', function (done) {
+                cp_plants_controller.plants_info.forEach(element => {
+                    expect(element).to.be.a('function');
+                });
+                done();
+            });
         });
 
         it("calling .plants_info without a plant name should result in an error", function (done) {
-            expect(()=>{cp_plants_controller.plants_info(null,Res_object)}).to.throw();
+            expect(() => { cp_plants_controller.plants_info(null, Res_object); }).to.throw();
             done();
         });
 
