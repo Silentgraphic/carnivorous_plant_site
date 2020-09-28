233f5d26a0746625c8ad6094f8c1a0974b87f3d2
silentgraphic <kierannolan1995@gmail.com>
2020-09-28 13:19:35 +0100

    Extracted database callback and sanitized req params

--- controllers/cp_plants_controller.js
+++ controllers/cp_plants_controller.js
@@ -1,20 +1,39 @@
 const PlantModel = require('../models/plant_info');
+const createError = require('http-errors');
 
 const { param } = require("express-validator");
+const { create } = require('../models/plant_info');
 
-function plants_info_database_callback(err, plant) {
-    if (err) return next(err);
+function plants_info_database_callback(err, plant, next) {
+    if (err) throw err;
+    if (plant === null) throw new Error('No plant named that');
 }
 
 exports.plants_home = (req, res, next) => {
     res.render('cp_plants_home');
 };
 
-exports.plants_info = async (req, res, next) => {
+exports.plants_info = [
     //Escape plant name
-    await param('plant_name').escape().run(req);
-    
-    PlantModel.findOne({ title: req.params.plant_name }, plants_info_database_callback);
-    
-    res.render('plant_info_layout', { plant_name: req.params.plant_name });
-};
+    param('plant_name')
+        .escape()
+    ,
+    //Database call
+    (req, res, next) => {
+        PlantModel.findOne({ title: req.params.plant_name },
+            (err, plant) => {
+                try {
+                    plants_info_database_callback(err, plant, next);
+                    next();
+                } catch (error) {
+                    if (error.message == 'No plant named that') return next(createError(404));
+                    else return next(error);
+                }
+            }
+        );
+    },
+    //Proccess request
+    (req, res, next) => {
+        res.render('plant_info_layout', { plant_name: req.params.plant_name });
+    }
+];
