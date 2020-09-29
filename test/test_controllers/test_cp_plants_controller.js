a52c13dd7bd0c83c8ac407141df7b9631ce02bed
silentgraphic <kierannolan1995@gmail.com>
2020-09-29 12:16:46 +0100

    Added  res tests and changed res, req to constructors

--- test/test_controllers/test_cp_plants_controller.js
+++ test/test_controllers/test_cp_plants_controller.js
@@ -7,24 +7,33 @@ chai.use(assertArrays);
 const cp_plants_controller = rewire('../../controllers/cp_plants_controller');
 
 describe('plants controller', function () {
-    var Req_object;
-    var Res_object;
+    //Setup a response object constructor
+    function Res_object () {
+        this.template = null,
+        this.options = null,
+        this.render = function (template, options) {
+            this.template = template;
+            this.options = options;
+        }
+    };
+
+    //Setup a request object
+    function Req_object() {
+        this.params = { plant_name: null }
+    };
+
+    var Render_vars;
 
     before(function () {
-        Res_object = {
-            template: null,
-            options: null,
-            render: function (template, options) {
-                this.template = template;
-                this.options = options;
-            }
-        };
-        Req_object = {
-            params: { plant_name: null },
-        };
         //Remove sanitizer as it an external dependency that doesn't need testing
         cp_plants_controller.plants_info = cp_plants_controller.plants_info.splice(1);
-        console.log(cp_plants_controller.plants_info);
+
+        //Setup variables for rendering
+        Render_vars = {
+            title: 'title',
+            plant_text: 'text',
+            images: ['/image/location1', '/image/location1']
+        };
     });
     describe('plants', function () {
         it('calling .plants_home should return render function', function (done) {
@@ -33,14 +42,14 @@ describe('plants controller', function () {
         });
 
         it('calling .plants_home should render cp_plants_home', function () {
-            var res = Res_object;
+            let res = new Res_object();
             cp_plants_controller.plants_home(null, res);
             expect(res.template).to.be.equal('cp_plants_home');
         });
     });
     describe('plants info', function () {
-        describe('plants_info should be an array of callback functions', function () {
-            it('calling .plants_info should return array of callbacks', function (done) {
+        describe('plants_info callback array', function () {
+            it('calling .plants_info should return an array', function (done) {
                 expect(cp_plants_controller.plants_info).to.be.an('array');
                 done();
             });
@@ -52,17 +61,39 @@ describe('plants controller', function () {
             });
         });
 
-        it("calling .plants_info without a plant name should result in an error", function (done) {
-            expect(() => { cp_plants_controller.plants_info(null, Res_object); }).to.throw();
+        it("calling .plants_info without a request should result in an error", function (done) {
+            expect(() => { cp_plants_controller.plants_info[1](null, new  Res_object()); }).to.throw();
             done();
         });
+        describe('Response object', function () {
+            let res;
+            let req;
 
-        it('calling .plants_info should render plant_info_layout', function () {
-            var res = Res_object;
-            var req = Req_object;
-            cp_plants_controller.plants_info(req, res);
-            expect(res.template).to.be.equal('plant_info_layout');
+            beforeEach('set up res', function () {
+                res = new Res_object();
+                req =  new Req_object();
+                cp_plants_controller.__set__("render_info", Render_vars);
+            });
+
+            afterEach(function () {
+                res = null;
+                req = null;
+            });
+            it('calling .plants_info should render plant_info_layout', function () {
+                cp_plants_controller.plants_info[1](req, res);
+                expect(res.template).to.be.equal('plant_info_layout');
+            });
+
+            it('.title should be a string', function () {
+                cp_plants_controller.plants_info[1](req, res);
+                expect(res.options.title).to.be.a('string');
+
+            });
+
+            it('.text should be a string', function () {
+                cp_plants_controller.plants_info[1](req, res);
+                expect(res.options.title).to.be.a('string');
+            });
         });
     });
-
 });
